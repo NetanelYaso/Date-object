dabf5276a3a50eb19f83b9694b671534971c35bd
NetanelYaso <netanelyaso12@gmail.com>
2022-05-24 09:15:14 +0300

    date-object

---
+++ main.js
@@ -0,0 +1,78 @@
+// function myDate(){
+// var mydate = new Date();
+//  console.log(mydate);
+// }
+
+//  function printStringDate(somedate){  
+//     somedate = new Date(somedate);
+//     console.log(somedate);
+//  }
+//  printStringDate('2/14/2022');
+
+// function someFunc() {
+//    var userdate = prompt('enter a date');
+//    console.log(new Date());
+// }
+
+// function printMonts() {
+//     var usermonth = prompt();
+//     return usermonth.getMonth();
+// }
+// printMonts();
+
+//  function printHours() {
+//      var userhour = prompt();
+//      return userhour.getHours();
+//  }
+//  printHours();
+
+// function printTheYear() {
+//     var useryear = prompt();
+//     return useryear.getFullYear();
+// }
+// printTheYear();
+
+// function printMinutes(){
+//     var userminuts = prompt();
+//    return userminuts.getMinutes();
+
+// }
+// printMinutes();
+
+// function twoDates(){
+//     var date1 = prompt();
+//     var date2 = prompt();
+//     if(date1<date2){
+//         return date1;
+//     }
+//     else {
+//         return date2;
+//     }
+// }
+
+// function printAge() {
+//     var datefromuser = prompt();
+//    var date = new Date().getFullYear() - new Date(datefromuser).getFullYear();  
+//    return date;
+// }
+
+// function AgeFromUser() {
+//     var mydate = new Date( prompt());
+//     if(mydate>18){
+//         return alert;
+//     }
+//     console.log('המשתמש לא רשאי');
+// }
+
+function printUserLocation() { 
+  var somedate = new Date();
+  console.log();
+}
+
+// function findRandomDate(){
+//     var rndMonth = Math.floor(Math.random()*12);
+//     var rndDay = Math.floor(Math.random()*31+1);
+//     var dateStr = rndMonth + "/" + rndDay + "/1996" ;
+//     var date = new Date(dateStr);
+//     return date;
+// }
